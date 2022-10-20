c0f0f0d86efec78c52ac6f1d39bc7317cc9cc8c3
preetibissoyi <Preetibissoyi@gmail.com>
2022-10-20 23:27:37 +0530

    todays assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,73 +1,130 @@
-const express = require('express');
+; const express = require('express');
 const router = express.Router();
 
-router.get('/students/:name', function(req, res) {
-    let studentName = req.params.name
-    console.log(studentName)
-    res.send(studentName)
+let arr1 = [1, 2, 3, 5, 6, 7, 8, 9]
+
+
+
+router.get("/sol1", function (req, res) {
+    const missing = [];
+
+    for (let i in arr1) {
+
+        let gap = arr1[i] - arr1[i - 1];
+
+        let diff = 1;
+
+        while (diff < gap) {
+            missing.push(arr1[i - 1] + diff);
+            diff++;
+        }
+    }
+    res.send(missing)
+
 })
 
-router.get("/random" , function(req, res) {
-    res.send("hi there")
+// router.get("/sol2", function (req, res) {
+//     let n = arr2.length -1
+//     let first = arr2[0]
+//     let last = arr2[arr2.length-1]
+//     let sum = (n*(first+last))/2
+//     let missing=0
+
+//     for (let i = 0; i<arr2.length; i++){
+//         missing = missing + arr2[i]
+
+//     }
+//     result =  missing - sum
+
+//     console.log(result);
+//     res.send(result);
+
+// })
+let arr2 = [33, 34, 35, 37, 38, 39]
+
+router.get("/sol2", function (req, res) {
+    const missing = [];
+
+    for (let i in arr2) {
+
+        let gap = arr2[i] - arr2[i - 1];
+
+        let diff = 1;
+
+        while (diff < gap) {
+            missing.push(arr2[i - 1] + diff);
+            diff++;
+        }
+    }
+    res.send(missing)
+
 })
 
+//ASSIGNMENT 2//
+let players = [
+    {
+        "name": "manish",
+        "dob": "1/1/1995",
+        "gender": "male",
+        "city": "jalandhar",
+        "sports": [
+            "swimming"
+        ]
+    },
+    {
+        "name": "lokesh",
+        "dob": "1/1/1990",
+        "gender": "male",
+        "city": "mumbai",
+        "sports": [
+            "soccer"
+        ]
+    },
+   { 
+    "name": "lokesh",
+    "dob": "1/1/1990",
+    "gender": "male",
+    "city": "mumbai",
+    "sports": [
+        "soccer"
+    ]
+},
+{
 
-router.get("/test-api" , function(req, res) {
-    res.send("hi FunctionUp")
-})
+"name": "lokesh",
+"dob": "1/1/1990",
+"gender": "male",
+"city": "mumbai",
+"sports": [
+    "soccer"
+]
+},
+
+{
+"name": "madhu",
+"dob": "11/10/1990",
+"gender": "female",
+"city": "raipur",
+"sports": [
+    "soccer"
+]
+},
+
+]
+
+router.post("/players",function (req,res){
+    console .log(players) 
+    let newplayer = req.body ;
+    let found = req.body.name;
+    console.log("new players found :-",newplayer)
+    for (i of players){
+      if (i.name===found){
+       return res.send("name already exists")  
+      } 
+    } 
+      
+    });
 
 
-router.get("/test-api-2" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API")
-})
-
-
-router.get("/test-api-3" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
-})
-
-
-router.get("/test-api-4" , function(req, res) {
-    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-
-
-router.get("/test-api-5" , function(req, res) {
-    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
-})
-
-router.get("/test-api-6" , function(req, res) {
-    res.send({a:56, b: 45})
-})
-
-router.post("/test-post", function(req, res) {
-    res.send([ 23, 45 , 6])
-})
-
-
-router.post("/test-post-2", function(req, res) {
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-router.post("/test-post-3", function(req, res) {
-    // let id = req.body.user
-    // let pwd= req.body.password
-
-    // console.log( id , pwd)
-
-    console.log( req.body )
-
-    res.send(  { msg: "hi" , status: true }  )
-})
-
-
-
-router.post("/test-post-4", function(req, res) {
-    let arr= [ 12, "functionup"]
-    let ele= req.body.element
-    arr.push(ele)
-    res.send(  { msg: arr , status: true }  )
-})
 
 module.exports = router;
