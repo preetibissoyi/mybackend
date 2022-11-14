fc4d92734a24d4d9863e516397175c7e01dfd4d7
monalisa <monalisaganguly620@gmail.com>
2022-11-14 15:44:55 +0530

    validation edited by mona

--- validator/validation.js
+++ validator/validation.js
@@ -6,9 +6,34 @@ const isValidName = function (name){
     return nameRegex.test(name);
 };
 //EMAIL VALIDATION 
-const isValidation = function(email) {
+const isValidEmail = function(email) {
     const emailRegex =
     /^[a-z0-9][a-z0-9-_\.]+@([a-z]|[a-z0-9]?[a-z0-9])\.[a-z0-9]{2,10}(?:\.[a-z]{2,10})?$/;
 };
 //PASSWORD VALIDATION
+const isValidPassword = function(password) {
+    const passwordRegex =
+    /^(?=.[A-Za-z])(?=.*\d)(?=,*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,15}$/;
+    return passwordRegex.test(password);
 
+};
+//VALUE VALIDATION
+const isEmpty = function (value){
+    if (typeof value === "undefined"|| value === null) return false;
+    if (typeof value ==="string"&& value.trim().length === 0) return false;
+    return true;
+};
+
+//ObjectId
+const isValidObjectId = (objectId) => {
+    return mongoose.Types.ObjectId.isValid(objectId);
+};
+
+module.exports = {
+    isEmpty,
+    isValidName,
+    isValidEmail,
+    isValidObjectId,
+    isValidPassword
+
+}
